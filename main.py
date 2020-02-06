4a76150bea607886adbb8f21feaa09ff81d8ad1e
Rishabh Ryber <rishabhryber@gmail.com>
2020-02-06 18:48:16 +0000

    basic functionality using github apis added

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
 import requests
 import json
-
 url = "https://api.github.com/users/rishabhryber/repos"
 r = requests.get(url)
-print(r.text)
+repos = json.loads(r.text)
+for repo in repos:
+  print(repo["name"])
+  print("\tDescription: "+repo["description"])
+  print("\tOwner: "+repo["owner"]["login"])
+  print("\tClone URL: "+repo["clone_url"])
+
+  print("\tForks Count: "+str(repo["forks_count"]))
+  print("\tWatch Count: "+repo["watchers_count"])
+  print("\tClone URL: "+repo["clone_url"])
+ # print("\tClone URL: "+repo["clone_url"])
