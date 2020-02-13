ab45bb8b1626a30a07cef107fae55d9e913efcc2
rishabh <rishabhryber@gmail.com>
2020-02-14 00:49:19 +0530

    interface for commandline inout added

---
+++ interface.py
@@ -0,0 +1,14 @@
+import sys
+import main
+
+if len(sys.argv)  < 3:
+    print("Invalid number of arguments passed:")
+    quit()
+type = sys.argv[1]
+if type=="-u" or type =="-U":
+    if sys.argv[2] == "-r" or  sys.argv[2] == "-r":
+        main.user_info(sys.argv[3],True)
+    else:
+        main.user_info(sys.argv[2],False)
+
+        
